26a2afa6a3d70b0ad65652fcd004baae34dbdfa7
avitalliob <avital.lub@gmail.com>
2021-04-23 16:56:34 +0300

    first commit

---
+++ client2.py
@@ -0,0 +1,20 @@
+import socket
+
+ClientSocket = socket.socket()
+host = '127.0.0.1'
+port = 2004
+
+print('Waiting for connection response')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+res = ClientSocket.recv(1024)
+while True:
+    Input = input('Hey there: ')
+    ClientSocket.send(str.encode(Input))
+    res = ClientSocket.recv(1024)
+    print(res.decode('utf-8'))
+
+ClientMultiSocket.close()
---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+from sqlite3 import Error
+import pandas as pd
+from pandas import DataFrame
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_connection(r"D:\Game_Launcher\DB.db")
+
+
