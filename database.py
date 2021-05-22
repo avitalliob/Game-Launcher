25e0bb6a65a0caefd972e481911c0689729fd1ec
avitalliob <avital.lub@gmail.com>
2021-05-22 20:46:46 +0300

    progress 1

--- database.py
+++ database.py
@@ -3,21 +3,203 @@ from sqlite3 import Error
 import pandas as pd
 from pandas import DataFrame
 
+DB_NAME = r"D:\Game_Launcher\usersDB.db"
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
+
+def create_connection():
+    """ create a database connection to a SQLite database
+    specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+     """
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
+        conn = sqlite3.connect(DB_NAME)
+        return conn
     except Error as e:
         print(e)
+
+    return conn
+
+
+def create_table(create_table_sql):
+    """
+    create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute(create_table_sql)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.commit()
+            conn.close()
+
+
+def create_user(username, password, salt):
+    """
+    create a new user to the user table
+    :param username:
+    :param password:
+    :param salt:
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        cur.execute('INSERT INTO users(username, password, salt) VALUES(?, ?, ?)', (username, password, salt))
+        print("User was created successfully")
+
+    except sqlite3.Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.commit()
+            conn.close()
+
+
+def delete_user(username):
+    """
+    remove user from USERS table in database
+    :param conn:
+    :param username:
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        cur.execute('DELETE from users where username = ?', (username, ))
+        print("User deleted successfully")
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.commit()
+            #conn.close()
+
+
+def change_password(username, new_password, new_salt):
+    """
+    change password for user in USERS table in database
+    :param username:
+    :param new_password:
+    :param new_salt:
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        cur.execute('UPDATE users set password = ? , SALT = ? where username = ?', (new_password, new_salt, username, ))
+
+        print("password changed successfully")
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.commit()
+            conn.close()
+
+
+def get_user(username):
+    """
+    returns a row in database for given username
+    if user doesnt exist returns None
+    :param username:
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        result = cur.execute("SELECT * FROM USERS WHERE username = ?", (username, ))
+
+        return result.fetchone()
+
+    except Error as e:
+        print(e)
+
     finally:
         if conn:
             conn.close()
 
 
+def get_password(username):
+    """
+    returns tuple (password, salt) for given username from database
+    if username doesnt exist returns None
+    :param username:
+    :return:
+    """
+    user = get_user(username)
+    if user is not None:
+        return user[1], user[2]
+    else:
+        return None
+
+
+def is_username_taken(username):
+    """
+    checks if given username is already taken
+    returns True if taken, False otherwise
+    :param username:
+    :return:
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        result = cur.execute("SELECT * FROM users WHERE username = ?", (username, ))
+
+        return len(list(result)) > 0
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
+
+
+def main():
+    #database = r"D:\Game_Launcher\DB.db"
+
+    sql_create_users_table = "CREATE TABLE IF NOT EXISTS users (" \
+                             "username TEXT PRIMARY KEY," \
+                             "password TEXT," \
+                             "salt TEXT" \
+                             ");"
+
+    #create a database connection
+    conn = create_connection()
+
+    #create table
+    if conn is not None:
+        #create users table
+        create_table(sql_create_users_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+
+
 if __name__ == '__main__':
-    create_connection(r"D:\Game_Launcher\DB.db")
+    main()
+
 
 
--- server.py
+++ server.py
@@ -1,19 +1,10 @@
 import socket
 import os
 from _thread import *
+import actions
+import database
+import passwords
 
-print('hello')
-ServerSocket = socket.socket()
-host = '127.0.0.1'
-port = 2004
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
-
-print('Socket is listening..')
-ServerSocket.listen(5)
 
 def multi_threaded_client(connection):
     connection.send(str.encode('Server is working:'))
@@ -26,13 +17,221 @@ def multi_threaded_client(connection):
         response = 'Server message: '
         if not data:
             break
-        connection.sendall(str.encode(send))
+        #connection.sendall(str.encode(send))
     connection.close()
 
-while True:
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(multi_threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSocket.close()
+
+def receive(sock):
+    """
+    receives and returns message from client
+    catch an error if connection brakes
+    :param sock:
+    :return:
+    """
+    input_line = None
+    try:
+        input_line = sock.recv(1024)
+    except:
+        print("Unexpected error")
+
+    return input_line
+
+
+def send(sock, message):
+    """
+    sends message through socket to client
+    catch an error if connection brakes
+    :param sock:
+    :param message:
+    :return:
+    """
+
+
+def register(sock):
+    """
+    register user func
+    create user in database
+    :param sock:
+    :return:
+    """
+    print("Registering...")
+
+    is_taken = True
+    username = None
+
+    while is_taken:
+        send(sock, actions.USERNAME_ACTION)
+        username = receive(sock)                           # get username
+        if not database.is_username_taken(username):        # check if username is free
+            is_taken = False
+        else:
+            sock.send("Username already taken, try something else")
+
+    # username is free
+
+    is_valid = False
+    password = None
+
+    while not is_valid:
+        send(sock, actions.PASSWORD_ACTION)
+        password = receive(sock)
+        send(sock, "Repeat password \n")
+        send(sock, actions.PASSWORD_ACTION)
+        password_repeat = receive(sock)
+        if password_repeat != password:
+            send(sock, "Passwords don't match, try again")
+            continue
+        if passwords.is_password_valid(password):
+            is_valid = True
+        else:
+            send(sock, "Password is invalid (should have more than 7 characters, at least one digit, "
+                      "one lowercase and one uppercase)")
+
+        # password is valid
+
+        hashed_password, salt = passwords.hash_password_generate_salt(password)
+        database.create_user(username, hashed_password, salt)
+
+        send(sock, "User succesfully registered! \nNow you can log in")
+
+
+def login(sock):
+    """
+    login user function
+    give an access for succesfully logged user
+    :param sock:
+    :return:
+    """
+    print("Login...")
+
+    sock.send(actions.USERNAME_ACTION)
+    username = receive(sock)
+
+    print(username)
+
+    hashed_password = None
+    salt = None
+    hash_and_salt = database.get_password(username)     # get salt and hashed password from db
+    if hash_and_salt:
+        hashed_password = hash_and_salt[0]
+        salt = hash_and_salt[1]
+
+    if not salt:                                        # user does not exist in db
+        salt = passwords.get_salt()                     # to not reveal if username exists or not
+                                                        # behave naturally with newly generated salt
+    nonce = passwords.get_salt()
+    send(sock, actions.NONCE_ACTION + ":" + salt + ":" + nonce)
+    send(sock, actions.PASSWORD_ACTION)
+    password = receive(sock)
+
+    if hashed_password is not None and passwords.check_password(password, nonce, hashed_password):
+        send(sock, "Successfully login")                # passwords matched
+        logged(sock, username)                                # access granted
+    else:
+        send(sock, "Userame or password incorrect")     # passwords do not match
+
+
+def change_password(sock, username):
+    """
+    change password user function
+    change password for user in db if everything succeed
+    :param sock:
+    :param username:
+    :return:
+    """
+    print("Changing password")
+
+    is_valid = False
+    password = None
+
+    while not is_valid:
+        send(sock, actions.PASSWORD_ACTION)
+        password = receive(sock)                                    # get password
+        send(sock, "repeat password \n")
+        send(sock, actions.PASSWORD_ACTION)
+        password_repeat = receive(sock)                             # get repeated password
+        if password_repeat != password:                             # compare them
+            send(sock, "Passwords do not match, try again")         # passwords not the same
+            continue                                                # prompt for passwords again
+        if passwords.is_password_valid(password):                   # passwords the same -> check if password is valid
+            is_valid = True
+        else:
+            send(sock, "Password is invalid (should have more than 7 characters,"    # pass invalid
+                          " at last one digit, one lowercase and one uppercase)")    # send validate pass rules
+        # password is valid
+
+        hashed_password, salt = passwords.hash_password_generate_salt(password)         # create hash
+        database.change_password(username, hashed_password, salt)           # change pass for user in db
+
+        send(sock, "Password successfully changed \nNow you can log in with a new one")     # confirm successful ction
+
+
+def logged(sock, username):
+    """
+    function to handle logged user
+    shows menu with actions for logged users
+    :param sock:
+    :param username:
+    :return:
+    """
+
+    send(sock, "Access granted!")
+
+    while True:
+        send(sock, "\nWhat do you want to do? (ls/change_password/logout/delete_account)")  # menu for logged user
+        send(sock, actions.TYPE_ACTION)
+        current_type = receive(sock)            # get type
+        if current_type is None:
+            print("Connection lost")
+            return
+        elif current_type == "change_password":
+            change_password(sock, username)
+        #elif current_type == "ls":
+            #send(sock, "root home etc lib media mnt")
+        elif current_type == "delete_account":
+            database.delete_user(username)
+            send(sock, "Your account was removed from the system")
+            return
+        elif current_type == "logout":
+            return
+        else:
+            send(sock, "unrecognized type")
+
+
+def run(sock):
+    """
+    main function when thread starts
+    to manage connection with client
+    :return:
+    """
+
+    send(sock, "Connected to server")
+
+    while True:
+        send(sock, "\nWhat do you want to do? (register/login/quit)")
+        send(sock, actions.TYPE_ACTION)
+        current_type = receive(sock)            # get type
+        if current_type is None:
+            break
+        elif current_type == "login":
+            login(sock)
+        elif current_type == "register":
+            register(sock)
+        elif current_type == "quit":
+            send(sock, actions.QUIT_ACTION)
+            break
+        else:
+            send(sock, "Unrecognized type")
+
+    # user quit from server
+    print("Client disconnected")
+    sock.close()
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
+
+
